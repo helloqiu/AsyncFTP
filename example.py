cea3b4afd9ca495f719c672693107d27989a95d4
Yuyang Zhang <helloqiu95@gmail.com>
2017-05-14 22:19:52 +0800

    change example

--- example.py
+++ example.py
@@ -8,6 +8,6 @@ from asyncftp.Logger import enable_pretty_logging
 if __name__ == "__main__":
     authorizer = BaseAuthorizer()
     authorizer.add_anonymous(os.path.join(os.getcwd(), "tests"), perm='elrwdf')
-    server = BaseServer(authorizer=authorizer, port=8888)
+    server = BaseServer(authorizer=authorizer, port=2122)
     enable_pretty_logging(server.logger)
     server.run()
